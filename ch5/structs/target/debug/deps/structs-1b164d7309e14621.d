e14b67b8e441376329e0d462fcd97a791a85af1d
Elizabeth Lui <elihylui@gmail.com>
2023-04-17 18:33:04 +0100

    Revert "init ch5"
    This reverts commit 51b429a9120ee2c1a569d47673ed7bbc9bba36fd.

--- ch5/structs/target/debug/deps/structs-1b164d7309e14621.d
+++
@@ -1,5 +0,0 @@
-/Users/elizabethlui/self-study/rust-lang-book/ch5/structs/target/debug/deps/structs-1b164d7309e14621: src/main.rs
-
-/Users/elizabethlui/self-study/rust-lang-book/ch5/structs/target/debug/deps/structs-1b164d7309e14621.d: src/main.rs
-
-src/main.rs:
--- ch5/structs/target/debug/structs.d
+++
@@ -1,1 +0,0 @@
-/Users/elizabethlui/self-study/rust-lang-book/ch5/structs/target/debug/structs: /Users/elizabethlui/self-study/rust-lang-book/ch5/structs/src/main.rs
